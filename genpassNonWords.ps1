1ff1166c74f1b376b2721718ab5e8c13260b8b30
dyerseve <dyerseve@gmail.com>
2024-05-21 16:52:22 -0400

    v2
    Made an alternative script to generate non word passwords.
    Once you get the shortcut on the desktop, you can pin it to position 2 and activate with WIN+2 shortcut.

---
+++ genpassNonWords.ps1
@@ -0,0 +1,28 @@
+$cmd = "C:\Programs\PwTech\pwtech.com"
+$args =  @("-profile", "Nonwords", "-gen", 1, "-readonly", "-silent")
+write-host = $cmd $args
+$output = & $cmd $args
+#$output = (& "C:\Programs\PwTech\pwtech.com" -profile Passphrase -gen 1 -readonly -silent)
+#$output.GetType()
+#$output | FL
+#| Select-Column -Index 0 -RequiredCount 1 | write-host
+
+
+# Check if the command executed successfully
+if ($?) {
+    # Split the output into lines
+    $outputLines = $output -split [Environment]::NewLine
+
+    # Check if there are at least 5 lines in the output
+    if ($outputLines.Length -ge 5) {
+        # Get the 5th line (index 4 since arrays are 0-based)
+        $lineToCopy = $outputLines[4]
+
+        # Copy the 5th line to the clipboard
+        Set-Clipboard -Value $lineToCopy
+    } else {
+        Write-Host "Output does not have at least 5 lines."
+    }
+} else {
+    Write-Host "Command execution failed."
+}
